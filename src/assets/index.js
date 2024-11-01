2ff9f94d6c0712aaf938b8b4978a69354ddac3b8
mfeti <mfeti2175@gmail.com>
2024-11-01 10:19:10 +0300

    ready the development by structuring the folder

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import CoffeeWhite from "./coffee-white.png";
+import Coffee2 from "./coffee2.png";
+
+export { CoffeeWhite, Coffee2 };
---
+++ src/assets/website/index.js
@@ -0,0 +1,15 @@
+import AppStore from "./app_store.png";
+import CoffeeLogo from "./coffee_logo.png";
+import CoffeeBeanBg from "./coffee-beans-bg.png";
+import CoffeeFooter from "./coffee-footer.jpg";
+import CoffeeTexture from "./coffee-texture.jpg";
+import PlayStore from "./play_store.png";
+
+export {
+  AppStore,
+  CoffeeBeanBg,
+  CoffeeLogo,
+  CoffeeTexture,
+  CoffeeFooter,
+  PlayStore,
+};
