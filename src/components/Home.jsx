1dabc26db1175a85d03bf767057b123ad3a4d1a6
mfeti <mfeti2175@gmail.com>
2024-11-02 09:38:14 +0300

    animation done in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,12 +8,15 @@ function Home() {
         <div className="flex md:flex-row flex-col-reverse">
           {/* text content */}
           <div className="md:w-1/2 flex flex-col justify-center gap-6">
-            <h1 className="text-5xl sm:text-6xl lg:text-7xl tracking-wide text-light font-bold">
+            <h1
+              data-aos="fade-up"
+              className="text-5xl sm:text-6xl lg:text-7xl tracking-wide text-light font-bold"
+            >
               We serve the richest{" "}
               <span className="font-cursive text-primary">Coffee</span> in the
               city
             </h1>
-            <div className="">
+            <div data-aos="fade-up" data-aos-delay="300" className="">
               <Button styles="ring-2 ring-primary bg-gradient-to-r from-primary to-secondary ">
                 Coffee And Code
               </Button>
@@ -27,13 +30,19 @@ function Home() {
               className="w-[300px] sm:w-[450px] sm:scale-110 spin"
             />
             {/* hey coder */}
-            <div className="absolute lg:top-5 lg:left-5 -top-3 left-0">
+            <div
+              data-aos="fade-up-left"
+              className="absolute lg:top-5 lg:left-5 -top-3 left-0"
+            >
               <Button styles="py-3 rounded-xl whitespace-nowrap bg-gradient-to-r from-primary to-secondary">
                 Hey Coder
               </Button>
             </div>
             {/* Best coffee  */}
-            <div className="absolute lg:bottom-5 bottom-0 lg:right-5 -right-2">
+            <div
+              data-aos="fade-down-right"
+              className="absolute lg:bottom-5 bottom-0 lg:right-5 -right-2"
+            >
               <Button styles="py-3 rounded-xl whitespace-nowrap bg-gradient-to-r from-primary to-secondary">
                 Best Coffee
               </Button>
